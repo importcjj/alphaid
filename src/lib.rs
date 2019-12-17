f7893f49ac1db0e61a36ed3db1a33eef3b7a8d86
George Chen <619507631@qq.com>
2019-12-17 13:44:02 +0800

    rustfmt

--- src/lib.rs
+++ src/lib.rs
@@ -1,20 +1,10 @@
 const MINLEN: usize = 6;
 const BASE: usize = 62;
-static ALPHABET: &'static [u8;BASE]= b"abcdefghijklmnopqrstuvwxyz0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-
-
-
-fn main() {
-    let x = encode(1).unwrap();
-    println!("{:?}", std::str::from_utf8(&x));
-    
-    let i = decode(b"baaaab").unwrap();
-    println!("{}", i);
-}
+static ALPHABET: &'static [u8; BASE] =
+    b"abcdefghijklmnopqrstuvwxyz0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
 #[derive(Debug)]
 pub struct EncodeError;
-
 
 #[derive(Debug)]
 pub struct DecodeError;
@@ -25,7 +15,7 @@ pub fn encode(mut n: i64) -> Result<Vec<u8>, EncodeError> {
     if pad > 0 {
         n += BASE.pow(pad as u32) as i64;
     }
-    
+
     let length = (n as f64).log(BASE as f64) as i64;
     for i in (0..=length).rev() {
         let bcp = BASE.pow(i as u32) as i64;
@@ -36,22 +26,21 @@ pub fn encode(mut n: i64) -> Result<Vec<u8>, EncodeError> {
     Ok(out)
 }
 
-pub fn decode(s: &[u8]) -> Result<i64, DecodeError> {    
-    let length= s.len();
+pub fn decode(s: &[u8]) -> Result<i64, DecodeError> {
+    let length = s.len();
     if length == 0 {
-        return Err(DecodeError)
+        return Err(DecodeError);
     }
 
     let mut r = 0;
     let l = length - 1;
-
 
     for i in (0..=l).rev() {
         let b = l - i;
         let bcp = BASE.pow(b as u32) as i64;
         match ALPHABET.iter().position(|&x| x == s[i]) {
             Some(t) => r += bcp * t as i64,
-            None => return Err(DecodeError)
+            None => return Err(DecodeError),
         }
     }
 
